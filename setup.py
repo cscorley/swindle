8f7676784ef59f16d2dafcf1ed12efea0398b2ee
Christopher Corley <cscorley@ua.edu>
2012-09-16 14:38:27 -0500

    Add setup.py and Makefile, ignore virtualenv

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='swindle',
+    version='0.0.1',
+    description='A LISP-like that throws away parentheses for Python-style indentation.',
+    long_description=readme,
+    author='Christopher S. Corley',
+    author_email='cscorley@gmail.com',
+    url='https://github.com/cscorley/swindle',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
