8a93d5b6c9460469c6aeb13ec312182be3396753
Christopher Corley <cscorley@ua.edu>
2012-09-24 19:25:56 -0500

    Improve type printing

--- swindle/lexeme.py
+++ swindle/lexeme.py
@@ -4,18 +4,20 @@ class Lexeme:
     def __init__(self, val, line_no, col_no, aux=0, unknown=False):
         self.val_type = Types(val)
         self.val = val
-        self.line_no = line_no
-        self.col_no = col_no
+        self.line_no = line_no + 1
+        self.col_no = col_no - len(str(val)) + 1
         self.aux = aux
         self.unknown = unknown
 
     def __str__(self):
         string = "(" + str(self.line_no)
         string += "," + str(self.col_no) + ")\t"
-        string += str(self.val_type) + "\t"
-        string += str(self.val)
+        string += str(self.val_type)
+        if not (self.val_type.keyword or self.val_type.punctuation
+                or self.val_type.whitespace):
+            string += "\t" + str(self.val)
         if self.aux:
-            string += ", " + str(self.aux)
+            string += "\taux=" + str(self.aux)
         if self.unknown:
-            string += ", " + str(self.unknown)
+            string += "\tUNKNOWN "
         return string
--- swindle/types.py
+++ swindle/types.py
@@ -1,32 +1,44 @@
+# keywords
+KEYWORDS = frozenset(["def",
+                    "lambda",
+                    "set!",
+                    "if",
+                    "else",
+                    "True",
+                    "False"
+                    ])
+# punctuation
+PUNCTUATION = frozenset(":[]()+-`")
+
+# variables
+# operators
+# numbers
+
 
 class Types:
     def __init__(self, val):
-        # keywords
-        keywords = frozenset(["def",
-                            "lambda",
-                            "set!",
-                            "if",
-                            "else",
-                            "True",
-                            "False"
-                            ])
-        # punctuation
-        punctuation = frozenset(":[]()+-`")
+        self.keyword = False
+        self.punctuation = False
+        self.whitespace = False
+        self.string = False
+        self.integer = False
+        self.variable = False
 
-        # variables
-        # operators
-        # numbers
-
-        if val in keywords:
+        if val in KEYWORDS:
+            self.keyword = True
             self.type_string = val
-        elif val in punctuation:
+        elif val in PUNCTUATION:
+            self.punctuation = True
             self.type_string = val
         else:
             if val.isdigit():
+                self.integer = True
                 self.type_string = "int"
             if val == " ":
+                self.whitespace = True
                 self.type_string = "ws"
             else:
+                self.variable = True
                 self.type_string = "var"
 
     def __str__(self):
