089bbcbc2713c2006aa9f69c52504d2f51cb92f1
Christopher Corley <cscorley@ua.edu>
2012-10-04 18:54:24 -0500

    Add lexeme left/right properties

--- swindle/lexeme.py
+++ swindle/lexeme.py
@@ -13,6 +13,10 @@ class Lexeme:
         self.aux = aux
         self.unknown = unknown
 
+        # For building parse trees
+        self.left = None
+        self.right = None
+
     def __str__(self):
         string = "(" + str(self.line_no)
         string += "," + str(self.col_no) + ")\t"
@@ -21,3 +25,25 @@ class Lexeme:
         if self.aux:
             string += "\taux=" + str(self.aux)
         return string
+
+    @property
+    def left(self):
+        return self._left_lexeme
+
+    @left.setter
+    def left(self, val):
+        if type(val) is Lexeme or val is None:
+            self._left_lexeme = val
+        else:
+            raise Exception("Left lexeme is not a Lexeme object")
+
+    @property
+    def right(self):
+        return self._right_lexeme
+
+    @right.setter
+    def right(self, val):
+        if type(val) is Lexeme or val is None:
+            self._right_lexeme = val
+        else:
+            raise Exception("Right lexeme is not Lexeme object")
